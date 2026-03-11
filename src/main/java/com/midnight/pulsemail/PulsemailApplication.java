34c142f308872930ee2cf89da0a5daf9190d4b95
midnightph <pedrorodacinski26@gmail.com>
2026-03-11 07:03:04 -0300

    first commit

---
+++ src/main/java/com/midnight/pulsemail/PulsemailApplication.java
@@ -0,0 +1,13 @@
+package com.midnight.pulsemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsemailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsemailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midnight/pulsemail/PulsemailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midnight.pulsemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsemailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

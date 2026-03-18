07b814380844da6d4626fb88681ee3fdd69770a2
midnightph <pedrorodacinski26@gmail.com>
2026-03-18 08:09:32 -0300

    Map User entity to users table explicitly
    Add the `@Table(name = "users")` annotation to ensure the `User` entity maps to the correct database table and avoid issues with reserved or inferred table names.

--- src/main/java/com/midnight/pulsemail/model/User.java
+++ src/main/java/com/midnight/pulsemail/model/User.java
@@ -5,10 +5,12 @@ import java.io.Serializable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "users")
 public class User implements Serializable {
     @Id
     @GeneratedValue
